879902477e31178030d3f531ca07b8f7ea8876a2
Massimo Cireddu <Massimo.Cireddu@ibm.com>
2022-05-03 12:04:27 +0200

    up

--- Distinct Columns to SQL/Form1.cs
+++ Distinct Columns to SQL/Form1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -30,20 +31,172 @@ namespace Distinct_Columns_to_SQL
             dt.Rows.Add("01", "7240", "1020");
             dt.Rows.Add("02", "8660", "0642");
             dt.Rows.Add("03", "1428", "0131");
+            dt.Rows.Add("04", "1428", "0131");
+            dt.Rows.Add("05", "1020", "7240");
 
             //change column order with SetOrdinal Method
             dt.Columns["ID"].SetOrdinal(0);
 
             //import data to the data grid
             dataGridView1.DataSource = dt;
+     
+            
 
-            //DISTINCT COLUMNS TO SQL
+            //DISTINCT COLUMNS TO SQL WITH ARRAYS
+            //user parameters
+            string column1 = "CODE1";
+            string column2 = "";
+            string query = "SELECT COMPANY_CD FROM TABLE WHERE COMPANY CODE IN (#COMPANY_LIST)"; //the target query which needs to be updated
+            string fieldToReplace = "#COMPANY_LIST"; //the field which has to be replaced within the SQL query
+            string output; //the output query
+
+
+            //incode parameters
+            string result = "";
+            string col1 = String.Empty;
+            string col2 = String.Empty;
+
+            //case if the input collection consists of only one column, then the col1 should be named as per that column
+            if (dt.Columns.Count == 1) //replace with input.Columns.Count
+            {
+                col1 = dt.Columns[0].ColumnName;
+
+                if (dt.Rows.Count > 0)
+                {
+                    //arrayList instance where to store distinct string elements
+                    string[] myList = new string[dt.Rows.Count];
+
+                    //first array element
+                    int k = 0;
+
+                    //datatable loop
+                    foreach (DataRow r in dt.Rows) //replace dt with input
+                    {
+                        myList[k] = r[col1].ToString();
+                        k++;
+                    }
+
+                    //get the distinct array
+                    string[] myListdist = myList.Distinct().ToArray();
+
+                    //loop arrayList and build a string with separators
+                    foreach (string obj in myListdist)
+                    {
+                        result += "'" + obj.ToString().Trim() + "',";
+                    }
+
+                    //result with seprator cleaning
+                    int i = result.LastIndexOf(',');
+                    result = result.Remove(i, 1);
+
+                }
+
+                else output = "''";
+            }
+
+            //case if the input colleciton has more than one column and the input columns are two
+            else if (dt.Columns.Count > 1 && column1 != "" && column2 != "")
+            {
+                //assign the input columns
+                col1 = column1;
+                col2 = column2;
+
+                if (dt.Rows.Count > 0)
+                {
+                    //arrayList instance where to store distinct string elements
+                    string[] myList = new string[dt.Rows.Count*2];
+
+                    //first array element
+                    int k = 0;
+
+                    //datatable loop
+                    foreach (DataRow r in dt.Rows) //replace dt with input
+                    {
+                        myList[k] = r[col1].ToString();
+                        myList[k+1] = r[col2].ToString();
+                       
+                        k = k + 2;
+                        
+                    }
+
+                    //get the distinct array
+                    string[] myListdist = myList.Distinct().ToArray();
+
+                    //loop arrayList and build a string with separators
+                    foreach (string obj in myListdist)
+                    {
+                        result += "'" + obj.ToString().Trim() + "',";
+                    }
+
+                    //result with seprator cleaning
+                    int i = result.LastIndexOf(',');
+                    result = result.Remove(i, 1);
+
+                }
+
+                else output = "''";
+            }
+
+            //case if the input colleciton has more than one column and the input columns is 1
+            else if (dt.Columns.Count > 1 && column1 != "" && column2 == "")
+            {
+
+                //assign the input column
+                col1 = column1;
+
+                if (dt.Rows.Count > 0)
+                {
+                    //arrayList instance where to store distinct string elements
+                    string[] myList = new string[dt.Rows.Count];
+
+                    //first array element
+                    int k = 0;
+
+                    //datatable loop
+                    foreach (DataRow r in dt.Rows) //replace dt with input
+                    {
+                        myList[k] = r[col1].ToString();
+                        k++;
+                    }
+
+                    //get the distinct array
+                    string[] myListdist = myList.Distinct().ToArray();
+
+                    //loop arrayList and build a string with separators
+                    foreach (string obj in myListdist)
+                    {
+                        result += "'" + obj.ToString().Trim() + "',";
+                    }
+
+                    //result with seprator cleaning
+                    int i = result.LastIndexOf(',');
+                    result = result.Remove(i, 1);
+
+                }
+
+                else output = "''";
+
+            }
+            else
+            {
+                output = "''";
+            }
+
+            //query update with distinct list
+            output = query.Replace(fieldToReplace, result);
+
+            /*
+            //DISTINCT COLUMNS TO SQL WITH ARRAYLIST
             //user parameters
             string column1 = "CODE1";
             string column2 = "CODE2";
             string query = "SELECT COMPANY_CD FROM TABLE WHERE COMPANY CODE IN (#COMPANY_LIST)"; //the target query which needs to be updated
             string fieldToReplace = "#COMPANY_LIST"; //the field which has to be replaced within the SQL query
             string output ; //the output query
+
+            
+            
+
 
             //incode parameters
             string result = "";
@@ -59,29 +212,93 @@ namespace Distinct_Columns_to_SQL
             //case if the input colleciton has more than one column and the input columns are two
             else if (dt.Columns.Count > 1 && column1 != "" && column2 != "")
             {
+                //assign the input columns
                 col1 = column1;
                 col2 = column2;
+
+                if (dt.Rows.Count > 0)
+                {
+                    //arrayList instance where to store distinct string elements
+                    ArrayList myList = new ArrayList();
+
+                    //datatable loop
+                    foreach (DataRow r in dt.Rows) //replace dt with input
+                    {
+                        //if element on r doesn't belong to arrayList, add to arrayList
+                        if (!myList.Contains(r[col1].ToString()))
+                        {
+                            myList.Add(r[col1].ToString());
+                        }
+                        if (!myList.Contains(r[col2].ToString()))
+                        {
+                            myList.Add(r[col2].ToString());
+                        }
+
+                    }
+
+                    //loop arrayList and build a string with separators
+                    foreach (object obj in myList)
+                    {
+                        result += "'" + obj.ToString().Trim() + "',";
+                    }
+
+                    //result with seprator cleaning
+                    int i = result.LastIndexOf(',');
+                    result = result.Remove(i, 1);
+
+                    //query update with distinct list
+                    output = query.Replace(fieldToReplace, result);
+
+                }
+
+                else output = "''";
             }
 
-            //hypothesis if the input colleciton has more than one column and the input columns is 1
+            //case if the input colleciton has more than one column and the input columns is 1
             else if (dt.Columns.Count > 1 && column1 != "" && column2 == "")
             {
+                
+                //assign the input column
                 col1 = column1;
 
-            }
-
-
-            if (dt.Rows.Count > 0)
-            {
-                foreach (DataRow r in dt.Rows) //replace dt with input
+                if (dt.Rows.Count > 0)
                 {
-                    result += "'" + r[col1].ToString().Trim() + "',";
+                    //arrayList instance where to store distinct string elements
+                    ArrayList myList = new ArrayList();
+
+                    //datatable loop
+                    foreach (DataRow r in dt.Rows) //replace dt with input
+                    {
+                        //if element on r doesn't belong to arrayList, add to arrayList
+                        if (!myList.Contains(r[col1].ToString()))
+                        {
+                            myList.Add(r[col1].ToString());
+                        }
+        
+                    }
+
+                    //loop arrayList and build a string with separators
+                    foreach(object obj in myList)
+                    {
+                        result += "'" + obj.ToString().Trim() + "',";
+                    }
+
+                    //result with seprator cleaning
+                    int i = result.LastIndexOf(',');
+                    result = result.Remove(i, 1);
+
+                    //query update with distinct list
+                    output = query.Replace(fieldToReplace, result);
+                    
                 }
-                int i = result.LastIndexOf(',');
-                result = result.Remove(i, 1);
-                output = result;
+                
+                else output = "''";
+
             }
-            else output = "''";
+            */
+
+
+
         }
     }
 }
